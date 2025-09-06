0f26e292766725a8bf54f824791215f112d01531
Chingkheinganba Thoudam <72967745+chingkhei-th@users.noreply.github.com>
2025-09-06 23:06:38 +0530

    feat: add new plugins for autocomplete

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -5,34 +5,34 @@ local servers = { "html", "cssls", "ts_ls", "pyright", "clangd", "marksman" }
 
 -- Setup servers with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = require("nvchad.configs.lspconfig").on_attach,
-    on_init = require("nvchad.configs.lspconfig").on_init,
-    capabilities = require("nvchad.configs.lspconfig").capabilities,
-  }
+	lspconfig[lsp].setup {
+		on_attach = require("nvchad.configs.lspconfig").on_attach,
+		on_init = require("nvchad.configs.lspconfig").on_init,
+		capabilities = require("nvchad.configs.lspconfig").capabilities,
+	}
 end
 
 -- Python specific config
 lspconfig.pyright.setup {
-  on_attach = require("nvchad.configs.lspconfig").on_attach,
-  on_init = require("nvchad.configs.lspconfig").on_init,
-  capabilities = require("nvchad.configs.lspconfig").capabilities,
-  settings = {
-    python = {
-      analysis = {
-        typeCheckingMode = "basic",
-      },
-    },
-  },
+	on_attach = require("nvchad.configs.lspconfig").on_attach,
+	on_init = require("nvchad.configs.lspconfig").on_init,
+	capabilities = require("nvchad.configs.lspconfig").capabilities,
+	settings = {
+		python = {
+			analysis = {
+				typeCheckingMode = "basic",
+			},
+		},
+	},
 }
 
 -- C/C++ specific config
 lspconfig.clangd.setup {
-  on_attach = require("nvchad.configs.lspconfig").on_attach,
-  on_init = require("nvchad.configs.lspconfig").on_init,
-  capabilities = require("nvchad.configs.lspconfig").capabilities,
-  cmd = {
-    "clangd",
-    "--offset-encoding=utf-16",
-  },
+	on_attach = require("nvchad.configs.lspconfig").on_attach,
+	on_init = require("nvchad.configs.lspconfig").on_init,
+	capabilities = require("nvchad.configs.lspconfig").capabilities,
+	cmd = {
+		"clangd",
+		"--offset-encoding=utf-16",
+	},
 }
--- lua/custom/plugins/autopairs.lua
+++ lua/custom/plugins/autopairs.lua
@@ -3,21 +3,22 @@
 --
 
 return {
-  -- testing mini pairs
-  enabled = false,
-  'windwp/nvim-autopairs',
-  event = 'InsertEnter',
-  -- Optional dependency
-  dependencies = { 'hrsh7th/nvim-cmp' },
-  config = function()
-    require('nvim-autopairs').setup {}
-    -- If you want to automatically add `(` after selecting a function or method
+	-- testing mini pairs
+	enabled = false,
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	config = true,
+	-- Optional dependency
+	dependencies = { "hrsh7th/nvim-cmp" },
+	config = function()
+		require("nvim-autopairs").setup {}
+		-- If you want to automatically add `(` after selecting a function or method
 
-    local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-    local cmp = require 'cmp'
-    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+		local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+		local cmp = require "cmp"
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
-    cmp.config.window.bordered()
-    cmp.config.window.bordered()
-  end,
+		cmp.config.window.bordered()
+		cmp.config.window.bordered()
+	end,
 }
