0e5ab4b3aa5c7735c3542aaab791bfd7779bbb20
chingkhei-th <72967745+chingkhei-th@users.noreply.github.com>
2026-02-07 19:25:27 +0530

    add `vim-sandwich` and `ReplaceWithRegister`

---
+++ lua/custom/plugins/editing.lua
@@ -0,0 +1,16 @@
+-- lua/custom/plugins/game_tutor.lua
+return {
+  -- Vim Sandwich (surround editing practice)
+  {
+    "machakann/vim-sandwich",
+    lazy = false,
+  },
+
+  -- ReplaceWithRegister (practice replacing text with registers)
+  {
+    "vim-scripts/ReplaceWithRegister",
+    lazy = false,
+  },
+
+}
+
